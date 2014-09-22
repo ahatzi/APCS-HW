268e6417ba8a73b53eccc70b8596d2dc31bf3005
Ari Hatzimemos <ArisComputer@Aris-MacBook-Pro.local>
2014-09-22 19:55:22 -0400

    Homework Number 4 String Stuff

---
+++ 04-StringStuff/Driver.java
@@ -0,0 +1,18 @@
+1. mixstart:
+
+public boolean mixStart(String str) {
+  return str.startsWith("ix",1);
+}
+
+2. makeOutWord:
+
+public String makeOutWord(String out, String word) {
+  return out.substring(0,2) + word + out.substring(2);
+}
+
+3. firstHalf:
+
+public String firstHalf(String str) {
+  int mid = str.length()/2;
+  return str.substring(0,mid);
+}
