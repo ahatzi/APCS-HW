d3e501e95a06adafde3f905be863b2eb3b4d3871
ahatzi <hatzimemosari3@gmail.com>
2014-09-23 08:12:23 -0400

    Update Driver.java

--- 04-StringStuff/Driver.java
+++ 04-StringStuff/Driver.java
@@ -1,16 +1,16 @@
-1. mixstart:
+// 1. mixstart:
 
 public boolean mixStart(String str) {
   return str.startsWith("ix",1);
 }
 
-2. makeOutWord:
+// 2. makeOutWord:
 
 public String makeOutWord(String out, String word) {
   return out.substring(0,2) + word + out.substring(2);
 }
 
-3. firstHalf:
+// 3. firstHalf:
 
 public String firstHalf(String str) {
   int mid = str.length()/2;
