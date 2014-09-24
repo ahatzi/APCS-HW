4fab350271bce1fc3919b9f61c33da5fa5c7616e
Ari Hatzimemos <ArisComputer@Aris-MacBook-Pro.local>
2014-09-24 01:27:43 -0400

    hw 5 Driver

---
+++ 05-CodingBat/Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+    public static void main(String[] args) {
+	codingbat a = new codingbat();
+	System.out.println(a.makeAbba("Hello","Hi"));
+	System.out.println("......Should be 'HelloHiHiHello'");
+	System.out.println(a.diff21(25));
+	System.out.println("......Should be 8");
+	System.out.println(a.nonStart("Hello","Hi"));
+        System.out.println("......Should be 'elloi'");
+    }
+} 
