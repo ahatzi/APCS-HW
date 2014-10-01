7d4d84784563af1ff23f15fd3fb7e107621eb0c8
Ari Hatzimemos <ArisComputer@Aris-MacBook-Pro.local>
2014-09-30 23:46:58 -0400

    HW 6

---
+++ 06-Strings/HW6.java
@@ -0,0 +1,66 @@
+public class HW6 {
+    public String frontTimes(String str, int n) {
+	String front;
+	if (str.length() < 3) {
+	    front = str;
+	}
+	else {
+	    front = str.substring(0,3);
+	}
+	String retstr = "";
+	while (n > 0) {
+	    retstr = retstr + front;
+	    n = n - 1;
+	}
+	return retstr;
+    }
+
+    public String stringBits(String str) {
+	int len = str.length();
+	int num = 0;
+	String retstr = "";
+	while (num < len) {
+	    String a = str.substring(num,num+1);
+	    retstr = retstr + a;
+	    if ((len - num) <= 2) {
+		num = len + 1;
+	    }
+	    else {
+		num = num + 2;
+	    }
+	}
+	return retstr;
+    }
+
+    public String stringYak(String str) {
+	int num = 0;
+	while (((str.length() - 1) - num) >= 2) {
+	    if (str.substring(num,num+3).equals("yak")) {
+		str = str.substring(0,num) + str.substring(num+3);
+	    }
+	    num = num + 1;
+	}
+	return str;
+    }
+
+    public int stringMatch(String a, String b) {
+	int mark = 0;
+	int num = 0;
+	int lena = a.length()-1;
+	int lenb = b.length()-1;
+	int len;
+	if ((lenb > lena)) {
+	    len = lena;
+	}
+	else {
+	    len = lenb;
+	}
+	while ((len - mark) > 0) {
+	    if (a.substring(mark,mark+2).equals(b.substring(mark,mark+2))) {
+		num = num + 1;
+	    }
+	    mark = mark + 1;
+       }
+       return num;
+   }
+}
---
+++ 06-Strings/HW6Driver.java
@@ -0,0 +1,9 @@
+public class HW6Driver {
+    public static void main(String[] args) {
+	HW6 a = new HW6();
+	System.out.println(a.frontTimes("hello",3));
+	System.out.println(a.stringBits("Hello"));
+	System.out.println(a.stringYak("xxxyakyyyakzzz"));
+	System.out.println(a.stringMatch("aaxxaaxx","iaxxai"));
+    }
+}
