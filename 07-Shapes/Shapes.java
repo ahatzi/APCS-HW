6f49bbeaebab900de1e45a6557ae4c7c1b78a91f
Ari Hatzimemos <hatzimemosari3@gmail.com>
2014-10-02 08:41:50 -0400

    Added new Shape methods

--- 07-Shapes/Shapes.java
+++ 07-Shapes/Shapes.java
@@ -36,4 +36,28 @@ public class Shapes {
 	}
 	return retstr;
     }
+
+    public String Tri3(int h) {
+	int row;
+	int num;
+	int star = 1;
+	int starCounter;
+	String retstr = "";
+	row = h;
+	while (row > 0) {
+	    for (num = (row*2-2)/2;num > 0;num = num - 1) {
+		retstr = retstr + " ";
+	    }
+	    for (starCounter = star;starCounter > 0;starCounter = starCounter - 1) {
+		retstr = retstr + "*";
+	    }
+	    for (num = (row*2-2)/2;num > 0;num = num - 1) {
+		retstr = retstr + " ";
+	    }
+	    retstr = retstr + "\n";
+	    row = row - 1;
+	    star = star + 2;
+	}
+	return retstr;
+    }
 }
--- 07-Shapes/ShapesDriver.java
+++ 07-Shapes/ShapesDriver.java
@@ -1,10 +1,14 @@
 public class ShapesDriver {
     public static void main(String[] args) {
 	Shapes a = new Shapes();
+
 	System.out.println(a.Tri1(4));
 	System.out.println(a.Tri1(6));
 
 	System.out.println(a.Tri2(6));
 	System.out.println(a.Tri2(4));
+
+	System.out.println(a.Tri3(4));
+        System.out.println(a.Tri3(6));
     }
 }
