749a9579fe2ca80f9f49f4df8659148505af40b5
Ari Hatzimemos <ArisComputer@Aris-MacBook-Pro.local>
2014-10-04 17:09:11 -0400

    HW 8

---
+++ 08-CodingBat/HW8Driver.java
@@ -0,0 +1,8 @@
+public class HW8Driver {
+    public static void main(String[] args) {
+	HW8 a = new HW8();
+	
+	System.out.println(a.stringSplosion("Code"));
+	System.out.println(a.stringX("Helxlxo"));
+    }
+}
