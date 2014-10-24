18000f132e99b5e7d7194c7086a1abb4b261e17c
Ari Hatzimemos <ArisComputer@Aris-MacBook-Pro.local>
2014-10-24 07:06:26 -0400

    13

--- 13-Arrays/arraysDriver.java
+++
@@ -1,8 +0,0 @@
-public class arraysDriver {
-    public static void main(String[] args) {
-	arrays a = new arrays();
-	System.out.println(a.frontPiece([1,2,3,4]));
-	System.out.println(a.sum13([1,2,13,4,2]));
-    }
-}
-	   
