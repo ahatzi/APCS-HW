0cbe108d6660161ba0cce28d5ca15f823c1679c7
Ari Hatzimemos <hatzimemosari3@gmail.com>
2014-11-07 08:19:54 -0500

    Our own list class

---
+++ sArray/sArray.java
@@ -0,0 +1,31 @@
+public class sArray{
+    private int[] data; //should be Object[]
+    
+    public sArray(){
+	//start array at size 100
+    }
+    
+    public int[] add(int i){ //add to end
+	
+    }
+
+    public int[] add(int index,int i){ //add to location index; shift everything else down
+
+    }
+
+    public int get(int index){
+
+    }
+
+    public int[] set(int index,int i){
+
+    }
+
+    public int size(){
+
+    }
+
+    public int[] remove(int index){
+
+    }
+}
