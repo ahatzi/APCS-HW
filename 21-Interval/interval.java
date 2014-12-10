42506e4a93fbee6305504cb2af2d5d05894f7ff3
Ari Hatzimemos <hatzimemosari3@gmail.com>
2014-12-09 20:33:30 -0500

    added compareTo

--- 21-Interval/interval.java
+++ 21-Interval/interval.java
@@ -22,12 +22,38 @@ public class interval{
         return "{"+low+","+high+"}";
     }
 
+    public int compareTo(interval other){
+	int low1 = this.low;
+	int low2 = other.low;
+	int high1 = this.high;
+	int high2 = other.high;
+	if ((low1 == low2)&&(high1 == high2)){
+	    return 0;
+	}
+	else if (low1 < low2){
+	    return -1;
+	}
+	else if (low1 > low2){
+	    return 1;
+	}
+	else if (high1 > high2){
+	    return 1;
+	}
+	else {
+	    return -1;
+	}
+    }
+
     public static void main(String[] args){
 	interval[] a = new interval[10];
 	for (int i=0;i<a.length;i++){
 	    a[i] = new interval();
 	}
-	System.out.println(Arrays.toString(a));
+	//System.out.println(Arrays.toString(a));
+	
+	interval x = new interval(9,30);
+	interval y = new interval(9,20);
+	System.out.println(y.compareTo(x));
     }
 
 }
